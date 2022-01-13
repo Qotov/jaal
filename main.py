a0ef06c9e710ebb33e9dc7a3a64a2f79072dfae4
Qotov <alexander.o.kotov@gmail.com>
2022-01-13 12:21:52 +0100

    add ectoderm table

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # import
 from jaal import Jaal
 from jaal.datasets import load_got
+import pandas as pd
 
 # load the data
-edge_df, node_df = load_got()
+edge_df = pd.read_csv('ectoderm_grn.csv')
 
 # define vis options
 vis_opts = {'height': '600px', # change height
@@ -14,5 +15,5 @@ vis_opts = {'height': '600px', # change height
 #Jaal(edge_df, node_df).plot(host="neucrest.herokuapp.com", port="8050", vis_opts=vis_opts)
 
 # init Jaal and run server (with gunicorn)
-app = Jaal(edge_df, node_df).create()
+app = Jaal(edge_df).create()
 server = app.server
