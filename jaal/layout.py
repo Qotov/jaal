b57552e403e4545070477b7ababf672c3b2503a4
Qotov <49810237+Qotov@users.noreply.github.com>
2022-01-13 11:55:33 +0100

    Update layout.py

--- jaal/layout.py
+++ jaal/layout.py
@@ -10,6 +10,7 @@ import visdcc
 import base64
 import pandas as pd
 # from dash import dcc, html
+from dash import html
 # import dash_core_components as dcc
 # import dash_html_components as html
 import dash_bootstrap_components as dbc
